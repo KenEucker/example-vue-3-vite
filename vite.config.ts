b7ddd9ad8e76497a9c494c9395cbbbd538c3aae8
Ken Eucker <keneucker@gmail.com>
2022-03-25 21:28:30 -0700

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,28 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: { charset: false, additionalData: `@import "./src/assets/styles/mixins.scss";` },
+  //     css: { charset: false },
+  //   },
+  // },
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/biketag.js`,
+        chunkFileNames: `assets/vendor.js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  // root: './public',
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
 })
